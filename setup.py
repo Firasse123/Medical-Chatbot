932eac31971ae57889ed1c64b268d52693fa239c
Firasse123 <firasguizani2012@gmail.com>
2025-11-17 19:39:59 +0100

    setup file added

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Medical_Chatbot",
+    version="0.0.0",
+    packages=find_packages(),
+    install_requires=[],
+    author="Firas Guizani",
+    author_email="firasguizani2012@gmail.com"
+)
+
+#It automatically finds all Python packages in the project starting from the same folder as setup.py.
+
+#A folder is considered a package if it contains:__init__.py
